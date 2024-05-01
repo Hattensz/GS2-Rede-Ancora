cd4736ad145f156e17cd8b6a83fe3fdbba6e6ca6
joaovbrocchi <joaobrocchiprofissional@gmail.com>
2024-05-01 10:47:22 -0300

    pagina-de-cadastro-pronta

--- src/pages/cadastro/CadastroPage.jsx
+++ src/pages/cadastro/CadastroPage.jsx
@@ -1,10 +1,15 @@
+import CadastroForm from "../../components/cadastro/CadastroForm";
 import Header3 from "../../components/header3/Header3";
+import {CenteredHeading} from "./stylesCadastroPage"
 
 export default function CadastroPage() {
     return (
         <div>
-            <Header3></Header3>
-            <h1>Cadastro</h1>
+            <Header3 />
+            <div className="centraliza">
+                <CenteredHeading>Cadastro</CenteredHeading>
+                <CadastroForm />
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/pages/cadastro/stylesCadastroPage.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const  CenteredHeading = styled.h1`
+    color: #003C60D1;
+    text-align: center;
+    margin-top: 0.5em;
+    margin-bottom: 1em;
+`;
